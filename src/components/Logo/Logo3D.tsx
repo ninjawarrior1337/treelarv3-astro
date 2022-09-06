bcdd9ab395d8acb5d1da3e2d436522dc492d3b4e
Treelar <t.rothled@gmail.com>
2022-09-06 01:04:04 -0700

    fix trying to render 3d on the edge

--- src/components/Logo/Logo3D.tsx
+++ src/components/Logo/Logo3D.tsx
@@ -1,17 +1,31 @@
 import { Environment, PerspectiveCamera } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
-import { Suspense } from "react"
+import { PropsWithChildren, Suspense, useEffect, useState } from "react"
 import { Model } from "./LogoModel"
+
+const ClientOnly: React.FC<PropsWithChildren<{}>> = ({children}) => {
+    const [loaded, setLoaded] = useState(false)
+
+    useEffect(() => {
+        setLoaded(true)
+    }, [])
+
+    return (
+        loaded ? <>{children}</> : null
+    )
+}
 
 export default function Logo3D() {
     return (
-        <Canvas>
-            <ambientLight intensity={0.1} />
-            <directionalLight intensity={1} position={[-1, 1.75, 1]}></directionalLight>
-            <PerspectiveCamera makeDefault position={[12, 10, 40]} />
-            <Suspense fallback={null}>
-                <Model></Model>
-            </Suspense>
-        </Canvas>
+        <ClientOnly>
+            <Canvas>
+                <ambientLight intensity={0.1} />
+                <directionalLight intensity={1} position={[-1, 1.75, 1]}></directionalLight>
+                <PerspectiveCamera makeDefault position={[12, 10, 40]} />
+                <Suspense fallback={null}>
+                    <Model></Model>
+                </Suspense>
+            </Canvas>
+        </ClientOnly>
     )
 }
