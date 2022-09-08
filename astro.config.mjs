7144f94b9d2e48fb309fa7a1c5ef33fb41359a1b
Treelar <t.rothled@gmail.com>
2022-09-07 23:32:49 -0700

    switch to static deployment for vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import cloudflare from "@astrojs/cloudflare"
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
