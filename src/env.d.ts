7c654560e38a9cd5df5aeff3ca1f43657c926cf0
Treelar <t.rothled@gmail.com>
2022-09-10 20:53:17 -0700

    add pusher for later

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="astro/client" />
 /// <reference types="unplugin-icons/types/svelte" />
+
+interface ImportMetaEnv {
+    readonly PUSHER_APP_ID: string;
+    readonly PUBLIC_PUSHER_APP_KEY: string;
+    readonly PUSHER_APP_SECRET: string;
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
